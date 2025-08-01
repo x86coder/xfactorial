a18012d99cd69a2968b827f0216c69d3271ed89c
Amed Reyes <reyesrodriguezjaime@gmail.com>
2025-08-01 12:40:03 -0600

    truncate input number

--- program.go
+++ program.go
@@ -8,11 +8,10 @@ import(
 	"runtime"
 )
 
-
 func main(){
-	fmt.Println("xfactorial v0.31 -- by x86coder");
+	fmt.Println("xfactorial v0.32 -- by x86coder");
 	if len(os.Args) < 2{
-		fmt.Println("Usage xfactorial {number}");
+		fmt.Println("Usage: xfactorial {natural_number_or_zero}");
 	} else{
 	
 		var factorial, targetNumber, unitSize, start float64
@@ -22,22 +21,25 @@ func main(){
 		processors = runtime.GOMAXPROCS(0)
 		targetNumber, err := strconv.ParseFloat(os.Args[1], 64)
 		
-		if err != nil || targetNumber < 0.0 || targetNumber < 1.0 {
+		if err != nil || targetNumber < 0.0{
 			fmt.Println("Error. Input argument not valid!");
-			fmt.Println("Usage xfactorial {natural_number_or_zero}");
+			fmt.Println("Usage: xfactorial {natural_number_or_zero}");
 		} else{
 		
+			targetNumber = math.Floor(targetNumber)
+			
 			/* Do not enter loop if machine is single-core */
 			if processors == 1 || targetNumber < 4.0{
 				// Stop value is 1.0, factorial(x) starts multiplying from above
 				factorial = factorial1(targetNumber, 1.0);
 			} else{
 				unitSize = math.Floor(targetNumber/float64(processors))
-				fmt.Println("[Unit of Execution] size = ", unitSize)
+				//fmt.Println("[Unit of Execution] size = ", unitSize)
 				
 				// Start each worker thread ...
 				start = 1.0
 				for i := 0; i<processors; i++{
+					// Wait for the last iteration to input/process the remainder from the division above (targetNumber/processors) e.g. If targetNumber is not even, then always will be a remainder because # of cores is mostly always multiple of 2.
 					if i == (processors-1){
 						fmt.Println(" > go routine() with: ", start, targetNumber);
 						go xfactorial(c, start, targetNumber);
